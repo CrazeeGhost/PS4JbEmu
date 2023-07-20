db88916b57ddc5362ef4f760d0bc4b0594281f68
CrazeeGhost <21267479+CrazeeGhost@users.noreply.github.com>
2023-07-20 14:34:50 -0400

    Changed update dir path

--- updateHost.sh
+++ updateHost.sh
@@ -2,6 +2,6 @@
 cd /home/pi/PS4JbEmu
 git pull
 cd - 2>&1 >/dev/null
-rsync -a --delete /home/pi/PS4JbEmu/900N/ /var/www/html/ps4
+rsync -a --delete /home/pi/PS4JbEmu/ /var/www/html/ps4
 chown -R www-data:www-data /var/www/html/ps4/*
 chmod -R 755 /var/www/html/ps4/*
