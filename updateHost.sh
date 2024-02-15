b6c77786b0b4ddcef44204c80cf978d4f1842cc9
CrazeeGhost <21267479+CrazeeGhost@users.noreply.github.com>
2024-02-15 17:16:22 -0500

    Removed all files to make room for PSFree

--- updateHost.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-cd /home/pi/PS4JbEmu
-git pull
-cd - 2>&1 >/dev/null
-rsync -a --delete /home/pi/PS4JbEmu/ /var/www/html/ps4
-chown -R www-data:www-data /var/www/html/ps4/*
-chmod -R 755 /var/www/html/ps4/*
