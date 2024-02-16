eeae562b7366cacb74bfa255c584b3ee1cbc6026
CrazeeGhost <21267479+CrazeeGhost@users.noreply.github.com>
2024-02-16 09:17:32 -0500

    Excluded README

--- updateHost.sh
+++ updateHost.sh
@@ -2,5 +2,5 @@
 cd /home/pi/PS4JbEmu
 git pull
 cd - 2>&1 >/dev/null
-rsync -a --delete --exclude=".git/" --exclude="updateHost.sh" /home/pi/PS4JbEmu/ /var/www/html/ps4
+rsync -a --delete --exclude=".git/" --exclude="updateHost.sh" --exclude="README.md" /home/pi/PS4JbEmu/ /var/www/html/ps4
 chown -R www-data:www-data /var/www/html/ps4/*
