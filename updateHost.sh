c5e8515b259b830e8c249864698b434cb9d7d653
CrazeeGhost <21267479+CrazeeGhost@users.noreply.github.com>
2024-02-16 08:58:14 -0500

    Added shell script to update the JB host files from github

---
+++ updateHost.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+cd /home/pi/PS4JbEmu
+git pull
+cd - 2>&1 >/dev/null
+rsync -a --delete --exclude=".git/" /home/pi/PS4JbEmu/ /var/www/html/ps4
+chown -R www-data:www-data /var/www/html/ps4/*
