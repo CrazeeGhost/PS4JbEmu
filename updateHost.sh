5fe3cbe3fb6ec8eaae36a512a4fcbbceb8214ce8
CrazeeGhost <21267479+CrazeeGhost@users.noreply.github.com>
2024-02-16 10:23:13 -0500

    Update updateHost.sh

--- updateHost.sh
+++ updateHost.sh
@@ -4,5 +4,6 @@ git pull
 cd - 2>&1 >/dev/null
 rsync -a --delete --exclude=".git/" --exclude="updateHost.sh" --exclude="README.md" /home/pi/PS4JbEmu/ /var/www/html/ps4
 chown -R www-data:www-data /var/www/html/ps4/*
+chmod 755 /var/www/html/ps4/script.php
 cp /home/pi/PS4JbEmu/updateHost.sh /home/pi
 chmod 755 /home/pi/updateHost.sh
