130f0f9b42a09e068bd52dadf06ffb647a398c00
CrazeeGhost <21267479+CrazeeGhost@users.noreply.github.com>
2024-11-13 10:42:58 -0500

    cleaned up for pppwn

--- updateHost.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-cd /home/pi/PS4JbEmu
-git pull
-cd - 2>&1 >/dev/null
-rsync -a --delete --exclude=".git/" --exclude="updateHost.sh" --exclude="README.md" /home/pi/PS4JbEmu/ /var/www/html/ps4
-chown -R www-data:www-data /var/www/html/ps4/*
-chmod 755 /var/www/html/ps4/script.php
-cp /home/pi/PS4JbEmu/updateHost.sh /home/pi
-chmod 755 /home/pi/updateHost.sh
